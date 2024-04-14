52e06919ff6f1b045c211462c740cda5c6f7b46c
Adilson Pedro <pedroadilson513@gmail.com>
2024-04-14 15:31:18 +0100

    algumas modificacoes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 let navbar = document.querySelector('.header .navbar');
+let formulario = document.querySelector("form");
+
+let nome = document.getElementById("nome");
+let assunto = document.getElementById("assunto");
+let email = document.getElementById("email");
+let numero =  document.getElementById("numero");
+let mensagem = document.getElementById("mensagem");
 
 document.querySelector('#menu-btn').onclick = () =>{
   navbar.classList.toggle('active');
@@ -10,9 +17,55 @@ window.onscroll = () =>{
   loginForm.classList.remove('active');
 }
 
+function limparCampos() {
+       
+    }
+
+
+function enviarEmail(){
+    const bodyMessage = `Nome: ${nome.value}<br> Email: ${email.value}<br>
+    Telefone: ${numero.value}<br>Mensagem: ${mensagem.value}`;
+    alert(nome.value + " " + assunto.value + " " + email.value + " " + mensagem.value);
+
+    Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "codekidsangola@gmail.com",
+    Password : "10A1EA03A9A4D1748168C7164F5D1B9E3DB7",
+    To : 'codekidsangola@gmail.com',
+    From : 'codekidsangola@gmail.com',
+    Subject : assunto.value,
+    Body : bodyMessage
+  }).then(
+    message => {
+      if(message === "OK"){
+            Swal.fire({
+            title: "Muito Obrigado!",
+            text: "O seu email foi enviado com sucesso, responderemos o mais breve possivel!",
+            icon: "success"
+          });
+      }else{
+        console.log(message);
+      }
+    }
+  );
+}
+formulario.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  if(!validarCampos(email.value)){  
+    Swal.fire({
+          title: "Email Inválido!",
+          text: "Por favor, digite um email válido",
+          icon: "warning"
+    });      
+  }else{
+    limparCampos();
+    enviarEmail();
+    formulario.reset();
+  }
+});
+
 function validarCampos(){
   if(!validateEmail(email.value)){
-    alert("Email Inválido!");
     return false;
   }
   return true;
